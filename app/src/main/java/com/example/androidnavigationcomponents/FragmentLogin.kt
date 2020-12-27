d29be95fe8737ef4aa36b5376864d279ba0e3f76
Muhammad Saqib <37036366+saqib00786@users.noreply.github.com>
2020-12-27 15:18:06 +0500

    3rd Empty commit

--- app/src/main/java/com/example/androidnavigationcomponents/FragmentLogin.kt
+++ app/src/main/java/com/example/androidnavigationcomponents/FragmentLogin.kt
@@ -5,18 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import kotlinx.android.synthetic.main.fragment_home.*
-import kotlinx.android.synthetic.main.fragment_search.*
-
-class FragmentHome : Fragment(R.layout.fragment_home) {
+class FragmentLogin : Fragment(R.layout.fragment_login) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        homeBtnID.setOnClickListener {
-            homeFragTextViewID.text = "Home Button Clicked!!"
-        }
 
     }
 
--- app/src/main/java/com/example/androidnavigationcomponents/FragmentSearch.kt
+++ app/src/main/java/com/example/androidnavigationcomponents/FragmentSearch.kt
@@ -5,11 +5,4 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class FragmentSearch : Fragment(R.layout.fragment_search) {
-    init {
-        Toast.makeText(context,"Fragment Search Open",Toast.LENGTH_SHORT)
-            .show()
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
 }
