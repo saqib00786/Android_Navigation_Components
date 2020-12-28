b280e1b83f0435fc051de64c60cf1063038a6b9b
Muhammad Saqib <37036366+saqib00786@users.noreply.github.com>
2020-12-28 16:42:02 +0500

    Monday 6th commit

--- app/src/main/java/com/example/androidnavigationcomponents/MainActivity.kt
+++ app/src/main/java/com/example/androidnavigationcomponents/MainActivity.kt
@@ -2,12 +2,34 @@ package com.example.androidnavigationcomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_containerID)
+                as NavHostFragment
 
+        navController = navHostFragment.findNavController()
+
+        setSupportActionBar(toolbarID)
+        setupActionBarWithNavController(navController)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
