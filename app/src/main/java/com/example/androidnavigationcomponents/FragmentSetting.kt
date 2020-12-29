55de96625c332e58792c0cd18ae7e7557545d112
Muhammad Saqib <37036366+saqib00786@users.noreply.github.com>
2020-12-29 15:43:46 +0500

    Tuesday 7th commit

---
+++ app/src/main/java/com/example/androidnavigationcomponents/FragmentSetting.kt
@@ -0,0 +1,16 @@
+package com.example.androidnavigationcomponents
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentSetting : Fragment(R.layout.fragment_setting) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
--- app/src/main/java/com/example/androidnavigationcomponents/MainActivity.kt
+++ app/src/main/java/com/example/androidnavigationcomponents/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.androidnavigationcomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -29,7 +33,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_nav_menu,menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController)
+                || super.onOptionsItemSelected(item)
+
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
 }
