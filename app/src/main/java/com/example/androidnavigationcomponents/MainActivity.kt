25cad03b20fcaa3ada7cd22c2e953b65d29cca29
Muhammad Saqib <37036366+saqib00786@users.noreply.github.com>
2020-12-30 16:42:55 +0500

    7th commit

--- app/src/main/java/com/example/androidnavigationcomponents/MainActivity.kt
+++ app/src/main/java/com/example/androidnavigationcomponents/MainActivity.kt
@@ -9,14 +9,13 @@ import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,27 +25,54 @@ class MainActivity : AppCompatActivity() {
                 as NavHostFragment
 
         navController = navHostFragment.findNavController()
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.fragmentHome, R.id.fragmentSearch),
+            navigation_drawerID
+        )
 
         setSupportActionBar(toolbarID)
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
+        navigationViewID.setupWithNavController(navController)
 
-
+        navigationViewID.setNavigationItemSelectedListener {
+            if(it.itemId == R.id.exitBtnID){
+//                super.onBackPressed()
+                finish()
+            }
+            true
+        }
     }
 
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.top_nav_menu,menu)
+        menuInflater.inflate(R.menu.top_nav_menu, menu)
         return true
 
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return item.onNavDestinationSelected(navController)
-                || super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.settingBtnID -> {
+                val action = NavGraphDirections.actionGlobalFragmentSetting()
+                navController.navigate(action)
+                true
+            }
+            R.id.aboutBtnID -> {
+                val action = NavGraphDirections.actionGlobalFragmentAbout()
+                navController.navigate(action)
+                true
+            }
+            else -> {
+                item.onNavDestinationSelected(navController)
+                        || super.onOptionsItemSelected(item)
+            }
+        }
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
